a9c04824c655b8ce031b210b594a65fd83411b03
kulnoor04 <kulnoor@ualberta.ca>
2025-09-26 14:05:03 -0600

    hello

---
+++ ode/app/src/main/java/com/example/ode/Diamond.java
@@ -0,0 +1,16 @@
+package com.example.quartz;
+
+
+public class Diamond extends Shape{
+    private final int y;
+    private final int x;
+
+
+    public Diamond(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+
+    }
+}
+
